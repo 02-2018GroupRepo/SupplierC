199b05c9a850685a92b66f71465d2cad8c164726
syntel <syntel@PHX440G3-2815Z9.syntelorg.com>
2018-04-10 10:22:31 -0400

    before juho merge

--- src/main/java/bootcamp/controller/InvoiceController.java
+++ src/main/java/bootcamp/controller/InvoiceController.java
@@ -1,6 +1,7 @@
 package bootcamp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +14,23 @@ public class InvoiceController {
 	@Autowired
 	InvoiceService invoiceService = new InvoiceService();
 	
+	// for finance
 	@RequestMapping("/operatingcash")
-	public double getOperatingCash() {
+	public double[] getOperatingCash() {
 		return invoiceService.getOperatingCash();
 	}
 	
-	@RequestMapping("invoice/receive")
+	// for the resupplier
+	@RequestMapping("/invoice/receive")
 	public void receiveInvoice(Invoice invoice) {
 		invoiceService.processInvoice(invoice);
 	}
+	
+	// get the money
+	@RequestMapping("/pay")
+	public void pay(double cash) {
+		invoiceService.pay(cash);
+	}
+	
+	
 }
--- src/main/java/bootcamp/service/InvoiceService.java
+++ src/main/java/bootcamp/service/InvoiceService.java
@@ -19,29 +19,37 @@ public class InvoiceService {
 		return cashOnHand;
 	}
 	
-	public double getOperatingCash() {
-		return 5000;
+	@Bean
+	public double[] getOperatingCash() {
+		return new double[] {cashOnHand,0d};
 	}
 	
+	
+	// process invoice from resupplier, not for vendors
 	public void processInvoice(Invoice invoice) {
 		for (InvoiceItem i : invoice.getItems()) {
 			invoice.setTotal(invoice.getTotal()
 					+(i.getCount()*i.getProduct().getRetail_price().longValueExact()));
 		}
-		if (invoice.getTotal() <= cashOnHand)
-			pay(invoice.getTotal());
-		else if (invoice.getTotal() > cashOnHand)
-			System.out.println("reject order"); //this is not good enough, come back to this!!
+		if (invoice.getTotal() <= cashOnHand) {
+			sendMoney(invoice.getTotal());
+		}
+		
+		else if (invoice.getTotal() > cashOnHand) {
+			System.out.println("reject order"); 
+			//this is not good enough, come back to this!!
+			}
 	}
 
-	//take in money
-	public void takeMoney(double newMoney) {
+	//take in money from vendors
+	public void pay(double newMoney) {
 		cashOnHand += newMoney;
 	}
 	
-	// pay for stuff
-	public void pay(double owed) {
-		cashOnHand -= owed;
+	// for us to pay resuppliers
+	public void sendMoney(double cashGoingOut) {
+		cashOnHand -= cashGoingOut;
+		// pay the resupplier right here
 	}
 	
 	
